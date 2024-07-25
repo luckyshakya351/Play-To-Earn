e02542cbea306e4106cec13ba22033821fa90875
anand <vermaanand278@gmail.com>
2024-07-25 13:57:18 +0530

    as

--- src/Shared/SocketContext.js
+++ src/Shared/SocketContext.js
@@ -1,24 +1,22 @@
 // SocketContext.js
-import React, { createContext, useContext, useMemo } from 'react';
-import io from 'socket.io-client';
-import { newDomain } from '../services/urls';
+import React, { createContext, useContext, useMemo } from "react";
+import io from "socket.io-client";
+import { domain } from "../services/urls";
 
 const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
- const socket =  useMemo(()=>io(newDomain),[])
+  const socket = useMemo(() => io(domain), []);
 
   return (
-    <SocketContext.Provider value={socket}>
-      {children}
-    </SocketContext.Provider>
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
   );
 };
 
 export const useSocket = () => {
   const socket = useContext(SocketContext);
   if (!socket) {
-    throw new Error('useSocket must be used within a SocketProvider');
+    throw new Error("useSocket must be used within a SocketProvider");
   }
   return socket;
 };
